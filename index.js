08b8d1ee88261c72b2c6ce56722044858ec94c36
Taine <taine.meyer@hotmail.com>
2022-06-13 10:53:33 +1000

    finish assignment

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Write your solution in this file!
+const employee = {
+    name: "Taine Meyer",
+    streetAddress: "12 Seagull Terrace"
+   };
+   function updateEmployeeWithKeyAndValue(employee, key, value) {
+       return Object.assign({}, employee, {[key]: value});
+   };
+   function destructivelyUpdateEmployeeWithKeyAndValue(){
+       employee.name = 'Sam';
+       employee.streetAddress = '12 Broadway';
+       return employee;
+   };
+   function deleteFromEmployeeByKey(employee, key) {
+       return Object.assign({}, delete employee.key);
+   };
+   function destructivelyDeleteFromEmployeeByKey (employee, key, value) {
+       employee[key] = value;
+       return employee;
+   };
